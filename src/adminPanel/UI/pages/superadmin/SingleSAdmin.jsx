58a4d5f70e1dbb03049a18115c93788463097b9e
RajOdedara4u <odedararaj4u@gmail.com>
2024-11-01 00:35:08 +0530

    library frontend

---
+++ src/adminPanel/UI/pages/superadmin/SingleSAdmin.jsx
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const SingleSAdmin = ({ index, name, email, _id }) => {
+  const navigate = useNavigate();
+  const delateuser = async () => {
+    try {
+      const response = await fetch("https://library-mern-back.onrender.com/delateuser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ _id }),
+      });
+
+      const res_data = await response.json();
+      if (response.ok) {
+        toast.success(res_data.message);
+        navigate("/");
+      }
+      if (!response.ok) {
+        toast.error(res_data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <tr className="odd:bg-white even:bg-gray-50 border-b border-gray-200">
+        <td className="px-6 py-4 font-medium text-gray-900 ">{index}</td>
+        <td className="px-6 py-4">{name}</td>
+        <td className="px-6 py-4">{email}</td>
+
+        <td className="px-6 py-4">
+          <button
+            onClick={delateuser}
+            className="font-medium text-red-600 hover:underline"
+          >
+            Delate
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+};
+
+export default SingleSAdmin;
